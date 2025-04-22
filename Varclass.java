ab9006d99f5ad84be405a2b372db2b6e4cbba332
sulocha88 <sulochayatageri@gmail.com>
2025-04-22 15:51:05 +0530

    save this file

---
+++ Varclass.java
@@ -0,0 +1,10 @@
+class Varclass{
+    public static void main(String[] args)
+    {
+        // Declared a Local Variable
+        int var = 10;
+
+        // This variable is local to this main method only
+        System.out.println("Local Variable: " + var);
+    }
+}

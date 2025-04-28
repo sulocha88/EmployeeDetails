04569f41d6c6a61de2f8567582efac8bf9fe4a5e
sulocha88 <sulochayatageri@gmail.com>
2025-04-28 14:28:44 +0530

    Save

---
+++ ArithmeticOperations.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+    }
+}
